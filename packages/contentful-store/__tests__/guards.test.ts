a7eb970ed9d05607ad14ab31fadaec2b621b4cd4
Leo Baker-Hytch <leo@insidelabs.tech>
2019-08-14 20:31:39 +0200

    Add tests for type guards

---
+++ packages/contentful-store/__tests__/guards.test.ts
@@ -0,0 +1,59 @@
+import { isAssetLink, isAssetLinkArray, isEntryLink, isEntryLinkArray } from '../src/lib/guards';
+
+describe('Type guards', () => {
+    const assetLink = {
+        sys: {
+            type: 'Link',
+            linkType: 'Asset',
+            id: 'KUurOhebmj5G5WVBgqFEw',
+        },
+    };
+
+    const entryLink = {
+        sys: {
+            type: 'Link',
+            linkType: 'Entry',
+            id: 'Ix9TWIHtIXK7fTXEu9hoO',
+        },
+    };
+
+    describe('isAssetLink', () => {
+        it('should validate an asset link', () => {
+            expect(isAssetLink(assetLink)).toBe(true);
+        });
+
+        it('should reject an entry link', () => {
+            expect(isAssetLink(entryLink)).toBe(false);
+        });
+    });
+
+    describe('isAssetLinkArray', () => {
+        it('should validate an array of asset links', () => {
+            expect(isAssetLinkArray([assetLink])).toBe(true);
+        });
+
+        it('should reject an array of entry links', () => {
+            expect(isAssetLinkArray([entryLink])).toBe(false);
+        });
+    });
+
+    describe('isEntryLink', () => {
+        it('should validate an entry link', () => {
+            expect(isEntryLink(entryLink)).toBe(true);
+        });
+
+        it('should reject an asset link', () => {
+            expect(isEntryLink(assetLink)).toBe(false);
+        });
+    });
+
+    describe('isEntryLinkArray', () => {
+        it('should validate an array of entry links', () => {
+            expect(isEntryLinkArray([entryLink])).toBe(true);
+        });
+
+        it('should reject an array of asset links', () => {
+            expect(isEntryLinkArray([assetLink])).toBe(false);
+        });
+    });
+});
