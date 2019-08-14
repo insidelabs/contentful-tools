1a4f880d0fc579e024515091ed43eb3018ff89ac
Leo Baker-Hytch <leo@insidelabs.tech>
2019-08-14 20:12:24 +0200

    Remove unused type guards

--- packages/contentful-store/src/lib/guards.ts
+++ packages/contentful-store/src/lib/guards.ts
@@ -1,8 +1,4 @@
-import { Asset, AssetLink, Entry, LinkType, SysType, EntryLink } from '../types';
-
-export function isAsset(value: unknown): value is Asset {
-    return isEntity(value) && value.sys.type === SysType.Asset;
-}
+import { AssetLink, Entry, LinkType, SysType, EntryLink } from '../types';
 
 export function isAssetLink(value: unknown): value is AssetLink {
     return isLink(value) && value.sys.linkType === LinkType.Asset;
@@ -12,20 +8,12 @@ export function isAssetLinkArray(value: unknown): value is AssetLink[] {
     return Array.isArray(value) && value.length > 0 && isAssetLink(value[0]);
 }
 
-export function isEntry(value: unknown): value is Entry<string> {
-    return isEntity(value) && value.sys.type === SysType.Entry;
-}
-
 export function isEntryLink(value: unknown): value is EntryLink<Entry<string>> {
     return isLink(value) && value.sys.linkType === LinkType.Entry;
 }
 
 export function isEntryLinkArray(value: unknown): value is EntryLink<Entry<string>>[] {
     return Array.isArray(value) && value.length > 0 && isEntryLink(value[0]);
-}
-
-function isEntity(value: unknown): value is { sys: { type: string } } {
-    return isRecord(value) && isRecord(value.sys) && typeof value.sys.type === 'string';
 }
 
 function isLink<L extends LinkType>(
