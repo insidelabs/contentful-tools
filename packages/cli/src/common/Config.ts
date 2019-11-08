af212d400452f80a1c797059859481ae299911a8
Leo Baker-Hytch <leo@insidelabs.tech>
2019-11-08 23:25:26 +0100

    Extract config schema

--- packages/cli/src/common/Config.ts
+++ packages/cli/src/common/Config.ts
@@ -1,5 +1,41 @@
+import * as t from 'io-ts';
 import { getConfigs } from './getConfigs';
 
 export type Config = Promised<ReturnType<typeof getConfigs>>[0];
 
 type Promised<T> = T extends Promise<infer R> ? R : never;
+
+export const required = t.interface({
+    space: t.string,
+    outDir: t.string,
+    locales: t.interface({
+        base: t.string,
+        extra: t.array(t.string),
+    }),
+    storeClass: t.string,
+});
+
+export const options = t.partial({
+    clean: t.boolean,
+    namespace: t.string,
+    localeOptional: t.boolean,
+    fieldGetters: t.array(t.string),
+    idField: t.string,
+    whitelist: t.boolean,
+    contentTypeNameMap: t.record(t.string, t.string),
+    singletons: t.array(t.string),
+    typeOverrides: t.record(
+        t.string,
+        t.record(
+            t.string,
+            t.interface({
+                path: t.string,
+                type: t.string,
+            }),
+        ),
+    ),
+});
+
+export const config = t.interface({
+    jobs: t.record(t.string, t.intersection([required, options])),
+});
--- packages/cli/src/common/getConfigs.ts
+++ packages/cli/src/common/getConfigs.ts
@@ -5,43 +5,7 @@ import * as t from 'io-ts';
 import { PathReporter } from 'io-ts/lib/PathReporter';
 import { isRight } from 'fp-ts/lib/Either';
 import { CommonFlags } from './BaseCommand';
-
-const required = t.interface({
-    space: t.string,
-    outDir: t.string,
-    locales: t.interface({
-        base: t.string,
-        extra: t.array(t.string),
-    }),
-    storeClass: t.string,
-});
-
-const options = t.partial({
-    clean: t.boolean,
-    namespace: t.string,
-    localeOptional: t.boolean,
-    fieldGetters: t.array(t.string),
-    idField: t.string,
-    whitelist: t.boolean,
-    contentTypeNameMap: t.record(t.string, t.string),
-    singletons: t.array(t.string),
-    typeOverrides: t.record(
-        t.string,
-        t.record(
-            t.string,
-            t.interface({
-                path: t.string,
-                type: t.string,
-            }),
-        ),
-    ),
-});
-
-const config = t.interface({
-    jobs: t.record(t.string, t.intersection([required, options])),
-});
-
-t.intersection([required, options]);
+import { config } from './Config';
 
 const debug = createDebugger('@contentful-tools/cli:config');
 
